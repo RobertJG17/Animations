0264d00861e8cba91e49df437dfec6cb1d717259
RobertJG17 <robertjguerra@yahoo.com>
2020-11-02 20:49:54 -0800

    Exploring the Functionality and Integrated Design of SwiftUI Animations

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Robert Guerra on 11/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount: CGFloat = 1
+    
+    var body: some View {
+        print(animationAmount)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
